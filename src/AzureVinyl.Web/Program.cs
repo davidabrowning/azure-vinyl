92bf21576a19bb1fbd0198d48246bb2a2121f0c7
David Browning <david.a.browning@gmail.com>
2026-05-19 13:46:29 +0200

    Configure Entra ID connection string

--- src/AzureVinyl.Web/Program.cs
+++ src/AzureVinyl.Web/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.AccessControl;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    var connection = new SqlConnection(connectionString);
+    connection.AccessToken = new DefaultAzureCredential()
+        .GetToken(
+            new AuthorizationRule.Core.TokenRequestContext(
+                new[] { "https://database.windows.net/.default" }
+            )
+        ).Token;
+        
+    options.UseSqlServer(connection);
+});
 
 var app = builder.Build();
 
