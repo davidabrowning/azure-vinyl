ca5677f296ca4639bd161c4aafca96240267e09b
David Browning <david.a.browning@gmail.com>
2026-05-19 13:33:39 +0200

    Add defaults

--- src/AzureVinyl.Web/Models/Vinyl.cs
+++ src/AzureVinyl.Web/Models/Vinyl.cs
@@ -1,8 +1,8 @@
 public class Vinyl
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Artist { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
     public int Year { get; set; }
-    public string AlbumArtUrl { get; set; }
+    public string AlbumArtUrl { get; set; } = string.Empty;
 }
