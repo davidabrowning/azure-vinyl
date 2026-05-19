6e994b2d2b692049a0161725fb8cc818e129c3da
David Browning <david.a.browning@gmail.com>
2026-05-19 13:17:12 +0200

    Add Vinyl entity

---
+++ src/AzureVinyl.Web/Models/Vinyl.cs
@@ -0,0 +1,8 @@
+public class Vinyl
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public int Year { get; set; }
+    public string AlbumArtUrl { get; set; }
+}
