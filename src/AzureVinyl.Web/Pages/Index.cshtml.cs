97a39482298bb41d84f668fc7e0c053b7b97c71c
David Browning <david.a.browning@gmail.com>
2026-05-19 15:36:46 +0200

    Fetch vinyls list from db

--- src/AzureVinyl.Web/Pages/Index.cshtml.cs
+++ src/AzureVinyl.Web/Pages/Index.cshtml.cs
@@ -6,14 +6,17 @@ namespace AzureVinyl.Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly AppDbContext _dbContext;
+    public List<Vinyl> Vinyls = new List<Vinyl>();
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, AppDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public void OnGet()
     {
-
+        Vinyls = _dbContext.Vinyls.ToList();
     }
 }
