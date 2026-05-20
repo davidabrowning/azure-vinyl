66a1e6b64e29d8c3526e6808991e44942001a23b
David Browning <david.a.browning@gmail.com>
2026-05-20 23:55:48 +0200

    Wire up Web to Api

--- src/AzureVinyl.Web/Pages/Index.cshtml.cs
+++ src/AzureVinyl.Web/Pages/Index.cshtml.cs
@@ -7,16 +7,20 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly AppDbContext _dbContext;
+    private readonly HttpClient _httpClient;
     public List<Vinyl> Vinyls = new List<Vinyl>();
+    public string VinylTestResult = string.Empty;
 
-    public IndexModel(ILogger<IndexModel> logger, AppDbContext dbContext)
+    public IndexModel(ILogger<IndexModel> logger, AppDbContext dbContext, HttpClient httpClient)
     {
         _logger = logger;
         _dbContext = dbContext;
+        _httpClient = httpClient;
     }
 
     public void OnGet()
     {
         Vinyls = _dbContext.Vinyls.ToList();
+        VinylTestResult = _httpClient.GetStringAsync("http://localhost:5067/api/vinyls").Result;
     }
 }
