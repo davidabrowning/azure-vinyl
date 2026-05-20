13d2f27bbf1acc7f339e1c5cce30dfaee08e48e9
David Browning <david.a.browning@gmail.com>
2026-05-21 00:23:18 +0200

    Fetch Vinyls from Api instead of Web

--- src/AzureVinyl.Api/Program.cs
+++ src/AzureVinyl.Api/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/vinyls", () =>
+app.MapGet("/api/vinyls", (AppDbContext appDbContext) =>
 {
-    return "Hello World!";
+    return appDbContext.Vinyls.ToList();
 });
 
 app.Run();
