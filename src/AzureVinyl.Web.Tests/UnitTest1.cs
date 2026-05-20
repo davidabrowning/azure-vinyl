9a763278cc7c4e3aae7f23bad061ab243e52d63b
David Browning <david.a.browning@gmail.com>
2026-05-20 22:22:12 +0200

    Add dummy test

--- src/AzureVinyl.Web.Tests/UnitTest1.cs
+++ src/AzureVinyl.Web.Tests/UnitTest1.cs
@@ -5,6 +5,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Arrange
+        int result;
 
+        // Act
+        result = 1 + 1;
+
+        // Assert
+        Assert.Equal(2, result);
     }
 }
