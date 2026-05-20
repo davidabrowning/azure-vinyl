2ef7e145de1c5961d48ee09869f17747c667a9a1
David Browning <david.a.browning@gmail.com>
2026-05-21 00:25:13 +0200

    Remove AppDbContext from Web

--- src/AzureVinyl.Web/Pages/Index.cshtml.cs
+++ src/AzureVinyl.Web/Pages/Index.cshtml.cs
@@ -6,14 +6,12 @@ namespace AzureVinyl.Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly AppDbContext _dbContext;
     private readonly HttpClient _httpClient;
     public List<Vinyl> Vinyls = new();
 
-    public IndexModel(ILogger<IndexModel> logger, AppDbContext dbContext, HttpClient httpClient)
+    public IndexModel(ILogger<IndexModel> logger, HttpClient httpClient)
     {
         _logger = logger;
-        _dbContext = dbContext;
         _httpClient = httpClient;
     }
 
