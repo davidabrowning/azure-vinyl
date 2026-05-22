3ec18cc27213e7ce90390844b7838e32e75e0d6f
David Browning <david.a.browning@gmail.com>
2026-05-22 23:46:32 +0200

    Add test project in correct location with failing test

---
+++ test/AzureVinyl.Web.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace AzureVinyl.Web.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void NewFailingTest()
+    {
+        // Arrange
+        int result;
+
+        // Act
+        result = 1 + 1;
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+}
