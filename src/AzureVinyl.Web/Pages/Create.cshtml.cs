c1fead91f30c80a2769a0cd50936df787bb97133
David Browning <david.a.browning@gmail.com>
2026-05-23 22:19:03 +0200

    Link web Create to api call

--- src/AzureVinyl.Web/Pages/Create.cshtml.cs
+++ src/AzureVinyl.Web/Pages/Create.cshtml.cs
@@ -5,8 +5,18 @@ namespace AzureVinyl.Web.Pages
 {
     public class CreateModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
         [BindProperty]
         public Vinyl Vinyl { get; set; } = new();
+
+        public CreateModel(ILogger<IndexModel> logger, IConfiguration configuration, HttpClient httpClient)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
 
         public void OnGet()
         {
@@ -19,7 +29,19 @@ namespace AzureVinyl.Web.Pages
                 return Page();
             }
 
-            // Save via api call here
+            try
+            {
+                string baseApiUrl = _configuration["ApiSettings:BaseUrl"];
+                var response = _httpClient.PostAsJsonAsync(baseApiUrl + "/api/vinyls", Vinyl).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogError("An error occurred while saving vinyl.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while saving vinyl.");
+            }
 
             return RedirectToPage("Index");
         }
